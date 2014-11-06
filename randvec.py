b23bd72bb82a1416c71c03116b9b6821aa29145c
blondegeek <tsmidt@berkeley.edu>
2014-11-05 16:47:27 -0800

    erased old things

--- randvec.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import itertools
-
-vec = 2*np.random.random_sample((3,1))-1
-norm = np.apply_along_axis(np.linalg.norm, 0, vec)
-ready = np.transpose(np.round(vec/(0.5*norm),2))
-ready2 = np.round(list(itertools.chain.from_iterable(ready)),2)
-out = ' '.join(map(str, ready2))
-print 'MAGMOM = ' + out + ' 6*0'
